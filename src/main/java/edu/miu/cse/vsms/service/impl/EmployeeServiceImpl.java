0a97cbdd6c4c66f0ae675bdd2d91174a1a3d1c78
sanjeevthapamiu <sanjeev.thapa@miu.edu>
2024-11-25 09:54:53 -0600

    Write Implementation for EmployeeServiceImpl

--- src/main/java/edu/miu/cse/vsms/service/impl/EmployeeServiceImpl.java
+++ src/main/java/edu/miu/cse/vsms/service/impl/EmployeeServiceImpl.java
@@ -3,14 +3,19 @@ package edu.miu.cse.vsms.service.impl;
 import edu.miu.cse.vsms.dto.request.EmployeeRequestDto;
 import edu.miu.cse.vsms.dto.response.EmployeeResponseDto;
 import edu.miu.cse.vsms.dto.response.VehicleServiceResponseDto;
+import edu.miu.cse.vsms.exception.ResourceNotFoundException;
 import edu.miu.cse.vsms.model.Employee;
 import edu.miu.cse.vsms.repository.EmployeeRepository;
 import edu.miu.cse.vsms.service.EmployeeService;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -21,22 +26,25 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public EmployeeResponseDto addEmployee(EmployeeRequestDto request) {
         // Write your code here
-
-        return null;
+        Employee employee = new Employee(request.name(), request.email(), request.phone(), request.hireDate());
+        Employee savedEmployee = employeeRepository.save(employee);
+        return mapToResponseDto(savedEmployee);
     }
 
     @Override
     public List<EmployeeResponseDto> getAllEmployees() {
         // Write your code here
-
-        return null;
+        return employeeRepository.findAll().stream()
+                .map(this::mapToResponseDto)
+                .collect(Collectors.toList());
     }
 
     @Override
     public EmployeeResponseDto getEmployeeById(Long id) {
         // Write your code here
-
-        return null;
+        return employeeRepository.findById(id)
+                .map(this::mapToResponseDto)
+                .orElseThrow(() -> new ResourceNotFoundException(String.format("Employee with id:%d Not Found", id)));
     }
 
     @Override
@@ -50,27 +58,29 @@ public class EmployeeServiceImpl implements EmployeeService {
             switch (key) {
                 case "name":
                     // Write your code here
-
+                    employee.setName((String) value);
                     break;
                 case "email":
                     // Write your code here
-
+                    employee.setEmail((String) value);
                     break;
                 case "phone":
                     // Write your code here
-
+                    employee.setPhone((String) value);
                     break;
                 case "hireDate":
                     // Write your code here
-
+                    employee.setHireDate((LocalDate) value);
                     break;
                 default:
                     throw new IllegalArgumentException("Invalid field: " + key);
             }
         });
-        // Write your code here
 
-        return null;
+        // Write your code here
+        Employee savedEmployee = employeeRepository.save(employee);
+
+        return mapToResponseDto(savedEmployee);
     }
 
     private EmployeeResponseDto mapToResponseDto(Employee employee) {
